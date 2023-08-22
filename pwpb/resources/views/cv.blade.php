c8847ba7a94fd8895aafb5f8e18ff23e09b27cd5
fslmc <fslfaisal17@gmail.com>
2023-08-22 13:54:38 +0700

    ts

---
+++ pwpb/resources/views/cv.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" rype="text/css" href="{{url('/css/cv.css')}}">
+    <title>CV saya</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="side">
+
+            <div class="image">
+                <img src="{{url('/assets/akukesoku.png')}}" alt="">
+            </div>
+            <div class="identity">
+                <div class="identity-item">
+                    <label for="">Nama</label>
+                    <span>Muhammad Faisal Aktsa</span>
+                </div>
+
+                <div class="identity-item">
+                    <label for="">Tanggal Lahir</label>
+                    <span>17 Maret 2007</span>
+                </div>
+
+                <div class="identity-item">
+                    <label for="">Alamat</label>
+                    <span>Kp. Mande, Rt/Rw. 2/2, Kec. Cimenyan, Desa. Cikadut, Kab. Bandung </span>
+                </div>
+
+                <div class="identity-item">
+                    <label for="">Kontak</label>
+                    <Span>No. Telp : 08696969</Span>
+                    <span>Email : fslf@gmail.com</span>
+                    <span>Insstagram : fo.safjifd</span>
+                    <span>Facebook : M Faisal Aktsa</span>
+                </div>
+            </div>
+            
+        </div>
+
+        <div class="main-content">
+
+            <div class="block">
+                <div class="title">PENDIDIKAN</div>
+                    <div class="box">
+                        <div class="box-item">
+                            <span>tamat sd</span>
+                            <span>2013-2019</span>
+                            <span>MI MIFTAHUL HUDA</span>
+                        </div>
+                        <div class="box-item">
+                            <span>tamat smp</span>
+                            <span>2019-2022</span>
+                            <span>MTS MIMHA</span>
+                        </div>
+                        <div class="box-item">
+                            <span>Esemka</span>
+                            <span>2022-</span>
+                            <span>SMKN 2 BANDUNG</span>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="block">
+                <div class="title">PENGALAMAN KERJA</div>
+                <div class="box">
+                    <div class="box-item">
+                        <span>Gapernah kerja</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="block">
+                <div class="title">SKILL</div>
+                <div class="box">
+                    <div class="box-item">
+                        <span>C++</span>
+                        <span>3/100</span>
+                    </div>
+                    <div class="box-item">
+                        <span>PHP</span>
+                        <span>5/100</span>
+                    </div>
+                    <div class="box-item">
+                        <span>HTML</span>
+                        <span>8/100</span>
+                    </div>
+                    <div class="box-item">
+                        <span>CSS</span>
+                        <span>2/100</span>
+                    </div>
+                    <div class="box-item">
+                        <span>Java</span>
+                        <span>1/100</span>
+                    </div>
+                    <div class="box-item">
+                        <span>Python</span>
+                        <span>0,5/100</span>
+                    </div>
+                    <div class="box-item">
+                        <span>Javascript</span>
+                        <span>0,1/100</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
